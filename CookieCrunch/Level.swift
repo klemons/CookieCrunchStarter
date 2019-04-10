84618cb27cad3c4d2fafc3d529e48b0ed4562e52
Kyle Lemons <kal18c@dmc-207-15.local>
2019-04-10 08:40:57 -0500

    added levels that have different shapes

--- CookieCrunch/Level.swift
+++ CookieCrunch/Level.swift
@@ -34,6 +34,26 @@ let numRows = 9
 class Level {
   private var cookies = Array2D<Cookie>(columns: numColumns, rows: numRows)
   
+  init(filename: String) {
+    // 1
+    guard let levelData = LevelData.loadFrom(file: filename) else { return }
+    // 2
+    let tilesArray = levelData.tiles
+    // 3
+    for (row, rowArray) in tilesArray.enumerated() {
+      // 4
+      let tileRow = numRows - row - 1
+      // 5
+      for (column, value) in rowArray.enumerated() {
+        if value == 1 {
+          tiles[column, tileRow] = Tile()
+        }
+      }
+    }
+  }
+
+  
+  
   func cookie(atColumn column: Int, row: Int) -> Cookie? {
     precondition(column >= 0 && column < numColumns)
     precondition(row >= 0 && row < numRows)
@@ -51,6 +71,7 @@ class Level {
     for row in 0..<numRows {
       for column in 0..<numColumns {
         
+        if tiles[column, row] != nil {
         // 2
         let cookieType = CookieType.random()
         
@@ -60,9 +81,19 @@ class Level {
         
         // 4
         set.insert(cookie)
+        }
       }
     }
     return set
+  }
+
+  //Describes level structure
+  private var tiles = Array2D<Tile>(columns: numColumns, rows: numRows)
+  
+  func tileAt(column: Int, row: Int) -> Tile? {
+    precondition(column >= 0 && column < numColumns)
+    precondition(row >= 0 && row < numRows)
+    return tiles[column, row]
   }
 
 
